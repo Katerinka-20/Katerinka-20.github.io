ab0c69ae721c383a54f959f33b0a3bfaaf8be40e
Katerinka-20 <160571425+Katerinka-20@users.noreply.github.com>
2024-03-29 10:45:23 +0000

    Fri, Mar 29, 2024, 5:45 PM +07:00

---
+++ extensions/extensions.py
@@ -0,0 +1,22 @@
+def main():
+    # Запрос имени файла у пользователя
+    file_name = input("File name: ")
+
+    # Определение типа файла по расширению
+    if file_name.lower().endswith(".gif"):
+        print("image/gif")
+    elif file_name.lower().endswith(".jpg") or file_name.lower().endswith(".jpeg"):
+        print("image/jpeg")
+    elif file_name.lower().endswith(".png"):
+        print("image/png")
+    elif file_name.lower().endswith(".pdf"):
+        print("application/pdf")
+    elif file_name.lower().endswith(".txt"):
+        print("text/plain")
+    elif file_name.lower().endswith(".zip"):
+        print("application/zip")
+    else:
+        print("application/octet-stream")
+
+if __name__ == "__main__":
+    main()
