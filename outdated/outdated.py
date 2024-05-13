3bc932b0413eadc6579460c630df4a34ae4f46e9
Katerinka-20 <160571425+Katerinka-20@users.noreply.github.com>
2024-05-13 14:59:24 +0000

    Mon, May 13, 2024, 9:59 PM +07:00

---
+++ outdated/outdated.py
@@ -0,0 +1,32 @@
+# Список месяцев
+months = ["январь", "февраль", "март", "апрель",
+          "май", "июнь", "июль", "август", "сентябрь", "октябрь",
+          "ноябрь", "декабрь"]
+
+# Функция для проверки правильности ввода даты
+def is_valid_date(date_str):
+    parts = date_str.split('.')
+    if len(parts) != 3:
+        return False
+    day, month, year = parts
+    if not (day.isdigit() and month.isdigit() and year.isdigit()):
+        return False
+    month = int(month)
+    if month < 1 or month > 12:
+        return False
+    return True
+
+# Цикл для ввода даты в старом формате
+while True:
+    date = input("Дата: ").lower()  # Приводим введенное значение к нижнему регистру
+    if is_valid_date(date):
+        break
+    else:
+        print("Неправильный формат даты. Попробуйте еще раз.")
+
+# Разбиваем введенную дату на составляющие
+day, month_str, year = date.split('.')
+month = months.index(month_str) + 1  # Получаем номер месяца
+
+# Форматируем вывод с нулем впереди при необходимости
+print(f"{year}-{month:02}-{day:02}")
