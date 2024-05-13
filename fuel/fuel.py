165626a359e699ffb307ff9e759484fdf309c8a2
Katerinka-20 <160571425+Katerinka-20@users.noreply.github.com>
2024-05-13 14:12:59 +0000

    Mon, May 13, 2024, 9:12 PM +07:00

---
+++ fuel/fuel.py
@@ -0,0 +1,29 @@
+def calculate_fuel_percentage(fraction):
+    try:
+        x, y = map(int, fraction.split('/'))
+        if y == 0 or x > y:
+            return None
+        percent = round((x / y) * 100)
+        if percent <= 1:
+            return 'E'
+        elif percent >= 99:
+            return 'F'
+        else:
+            return percent
+    except ValueError:
+        return None
+    except ZeroDivisionError:
+        return None
+
+def main():
+    while True:
+        fraction_input = input("Дробь: ")
+        result = calculate_fuel_percentage(fraction_input)
+        if result is not None:
+            print(result)
+            break
+        else:
+            print("Пожалуйста, введите корректную дробь.")
+
+if __name__ == "__main__":
+    main()
