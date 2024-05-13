ce26a17004e96699084c0447ecfa704a0ae8da1c
Katerinka-20 <160571425+Katerinka-20@users.noreply.github.com>
2024-05-13 15:08:12 +0000

    Mon, May 13, 2024, 10:08 PM +07:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -2,46 +2,34 @@ months = ["январь", "февраль", "март", "апрель",
           "май", "июнь", "июль", "август", "сентябрь", "октябрь",
           "ноябрь", "декабрь"]
 
-# Функция для проверки правильности ввода даты
-def is_valid_date(date_str):
-    parts = date_str.split('.')
-    if len(parts) != 3:
-        return False
-    day, month, year = parts
-    if not (day.isdigit() and month.replace(' ', '').isdigit() and year.isdigit()):
-        return False
-    month = int(month)
-    if month < 1 or month > 12:
-        return False
-    return True
+def get_iso_date(date_str):
+    parts = date_str.split()
+    if len(parts) == 3:
+        day, month, year = parts
+        if month.isdigit():
+            month = int(month)
+            if 1 <= month <= 12:
+                month = months[month - 1]
+            else:
+                return None
+        else:
+            month = month.lower()
+            if month not in months:
+                return None
+        month_index = months.index(month) + 1
+        return f"{year}-{month_index:02d}-{int(day):02d}"
+    else:
+        return None
 
-# Функция для получения номера месяца
-def get_month_number(month_str):
-    try:
-        return int(month_str.replace(' ', ''))
-    except ValueError:
-        return months.index(month_str.lower()) + 1
-
-# Цикл для ввода даты в старом формате
-while True:
-    try:
-        date = input("Дата: ").lower()  # Приводим введенное значение к нижнему регистру
-        if is_valid_date(date):
+def main():
+    while True:
+        date_input = input("Дата: ")
+        iso_date = get_iso_date(date_input)
+        if iso_date:
+            print(iso_date)
             break
         else:
-            print("Неправильный формат даты. Попробуйте еще раз.")
-    except EOFError:  # Пользователь нажал control-d
-        print("Программа завершена.")
-        exit()
+            print("Неправильный формат. Попробуйте еще раз.")
 
-# Разбиваем введенную дату на составляющие
-day, month_str, year = date.split('.')
-month = get_month_number(month_str)  # Получаем номер месяца
-
-# Проверка на корректность дня
-if int(day) < 1 or int(day) > 31:
-    print("Неправильный формат даты. Попробуйте еще раз.")
-    exit()
-
-# Форматируем вывод с нулем впереди при необходимости
-print(f"{year}-{month:02}-{day:02}")
+if __name__ == "__main__":
+    main()
